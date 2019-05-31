dc78e37a3af967e86878124e7bd8eaae4d2d0fbb
therealyingtong <yingtong@ethereum.org>
2019-05-31 16:19:18 +0700

    remove Vue line from config/dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,6 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
-Vue.config.silent = true;
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
